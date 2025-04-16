a631a3892f6f12e56baf11c1fc955c788fdd3072
Yoshiaki Kawazu <kawazzz@gmail.com>
2025-04-16 12:17:40 +0900

    Initial Commit

---
+++ CursorFinder/CursorFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CursorFinderApp.swift
+//  CursorFinder
+//
+//  Created by Yoshiaki Kawazu on 2025/04/16.
+//
+
+import SwiftUI
+
+@main
+struct CursorFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
